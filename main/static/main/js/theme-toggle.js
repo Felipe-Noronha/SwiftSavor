e5a3375e815b7667e64421fd54e84f65d69138e7
felipe <folopson@gmail.com>
2024-03-16 09:05:45 -0300

    css novo

--- main/static/main/js/theme-toggle.js
+++ main/static/main/js/theme-toggle.js
@@ -3,28 +3,42 @@ function toggleTheme() {
     if (theme === 'light') {
         document.body.classList.remove('light-theme');
         localStorage.setItem('theme', 'dark');
-        const oldThemeStyle = document.getElementById('theme-style');
-        if (oldThemeStyle) {
-            oldThemeStyle.remove();
-        }
-        const darkThemeLink = document.createElement('link');
-        darkThemeLink.rel = 'stylesheet';
-        darkThemeLink.type = 'text/css';
-        darkThemeLink.href = '/static/main/css/styles.css';
-        darkThemeLink.id = 'theme-style';
-        document.head.appendChild(darkThemeLink);
     } else {
         document.body.classList.add('light-theme');
         localStorage.setItem('theme', 'light');
-        const oldThemeStyle = document.getElementById('theme-style');
-        if (oldThemeStyle) {
-            oldThemeStyle.remove();
-        }
+    }
+}
+
+function toggleThemeStyles() {
+    let theme = localStorage.getItem('theme');
+    const oldThemeStyle = document.getElementById('theme-style');
+    if (oldThemeStyle) {
+        oldThemeStyle.remove();
+    }
+
+    if (theme === 'light') {
         const lightThemeLink = document.createElement('link');
         lightThemeLink.rel = 'stylesheet';
         lightThemeLink.type = 'text/css';
         lightThemeLink.href = '/static/main/css/styles-light.css';
         lightThemeLink.id = 'theme-style';
         document.head.appendChild(lightThemeLink);
+    } else {
+        const darkThemeLink = document.createElement('link');
+        darkThemeLink.rel = 'stylesheet';
+        darkThemeLink.type = 'text/css';
+        darkThemeLink.href = '/static/main/css/styles.css';
+        darkThemeLink.id = 'theme-style';
+        document.head.appendChild(darkThemeLink);
     }
+}
+
+toggleThemeStyles();
+
+const themeButton = document.getElementById('theme-button');
+if (themeButton) {
+    themeButton.addEventListener('click', function() {
+        toggleTheme();
+        toggleThemeStyles();
+    });
 }
